6402c6cbe4ceba86e8f264751c0619c472ecb52d
Florian Abel <mail@florianabel.com>
2022-07-10 19:29:14 +0200

    Creating the endpoints

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+import code
+from django.urls import path, re_path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    re_path('api/register-by-access-token/' + r'social/(?P<backend>[^/]+)/$', views.register_by_access_token),
+    path('api/authentication-test/', views.authentication_test),
+]
