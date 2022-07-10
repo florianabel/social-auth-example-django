bae00b28af97e1e6c893ea8621bd92ad39dc92af
Florian Abel <mail@florianabel.com>
2022-07-10 19:25:15 +0200

    Settings for DRF

--- social_auth_example_django/settings.py
+++ social_auth_example_django/settings.py
@@ -85,6 +85,20 @@ DATABASES = {
 }
 
 
+# Django Rest Framework
+# https://www.django-rest-framework.org/
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
